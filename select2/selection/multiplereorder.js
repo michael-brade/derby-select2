45fe078b72c7be9f1df18aaa0ad63292b10100ed
Michael Brade <brade@kde.org>
2016-09-17 17:48:16 +0200

    reordering selections: use Sortable directly, don't reorder twice
    * no jQuery dependency
    * need to undo Sortable changes because Derby does them as well

--- select2/selection/multiplereorder.js
+++ select2/selection/multiplereorder.js
@@ -7,18 +7,14 @@ module.exports = MultipleReorderSelection
  */
 function MultipleReorderSelection() {}
 
-// MultipleReorderSelection.prototype.init = function(decorated, model) {
-//     decorated.call(this, model);
-// };
 
 MultipleReorderSelection.prototype.create = function(decorated, model, dom) {
     decorated.call(this, model, dom);
-
-    require('jquery.sortable');
-    var $selection = this.$selection.find('.select2-selection__rendered');
-
     var _this = this;
-    $selection.sortable({
+
+    var Sortable = require('Sortable');
+
+    Sortable.create(this.selection.getElementsByClassName('select2-selection__rendered')[0], {
         animation: 200,
         filter: '.select2-search',
         onStart: function(evt) {},
@@ -27,10 +23,23 @@ MultipleReorderSelection.prototype.create = function(decorated, model, dom) {
             return evt.related.className.indexOf('select2-search') === -1;
         },
         onUpdate: function(evt) {
+        },
+        onSort: function(evt) {
             if (evt.oldIndex === evt.newIndex) {
                 return;
             }
 
+            // temporarily undo Sortable changes...
+            var parent = evt.item.parentElement;
+            var sibling = parent.getElementsByClassName('select2-selection__choice')[evt.oldIndex];
+
+            if (evt.oldIndex > evt.newIndex) {
+                sibling = sibling.nextSibling;
+            }
+
+            parent.insertBefore(evt.item, sibling);
+
+            // ...now Derby will redo it natively
             _this.emit('move', {
                 originalEvent: evt,
                 oldIndex: evt.oldIndex,
