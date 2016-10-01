6b55f82158ece8de0c6ab053febc48fa4c1b253d
Michael Brade <brade@kde.org>
2016-10-01 23:23:48 +0200

    reordering selections: make reodering possible again, keep focus
    Stop propagation of mousedown so that core cannot cancel it (to prevent the blur event).
    Then the dragging becomes possible again, but blur also fires. So set the focus again
    after the drop or the mouseup event.

--- selection/multiplereorder.js
+++ selection/multiplereorder.js
@@ -1,17 +1,31 @@
 /**
  *  MultipleReorderSelection: allow reodering a selection via drag&drop
  */
-export default (superclass) => class extends superclass {
+export default (superclass) => class extends superclass
+{
 
     create(model, dom) {
         if (super.create)
             super.create(model, dom);
+
+        this.$selection.on('mousedown', '.select2-selection__choice', evt => {
+            evt.stopPropagation();  // stop the event from being cancelled by core so that dragging becomes possible...
+        });
+
+        this.selection.addEventListener('mouseup', evt => {
+            // ...but focus again on mouseup
+            this.core.focus();
+        });
+
 
         const Sortable = require('Sortable');
 
         Sortable.create(this.selection.getElementsByClassName('select2-selection__rendered')[0], {
             animation: 200,
             filter: '.select2-search',
+            onEnd: evt => {
+                this.core.focus();
+            },
             onMove: evt => {
                 return evt.related.className.indexOf('select2-search') === -1;
             },
