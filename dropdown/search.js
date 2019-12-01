2abd3ff744b171f6497b75610e81420977922652
Michael Brade <brade@kde.org>
2019-12-01 23:27:03 +0100

    dropdown/search: convert to ES6 and derby views

--- dropdown/search.js
+++ dropdown/search.js
@@ -1,112 +1,106 @@
+import path from 'path';
 var $ = require('jquery');
 
-function Search() {}
+export default class Search
+{
+    static view = {
+        is: 'dropdown:search',
+        file: path.join(__dirname, 'search.html')
+    }
 
-Search.prototype.render = function(decorated) {
-    var $rendered = decorated.call(this);
+    init(model) {
+        this.options = this.parent.options;
+    }
 
-    var $search = $(
-      '<span class="select2-search select2-search--dropdown">' +
-        '<input class="select2-search__field" type="search" tabindex="-1"' +
-        ' autocomplete="off" autocorrect="off" autocapitalize="none"' +
-        ' spellcheck="false" role="searchbox" aria-autocomplete="list" />' +
-      '</span>'
-    );
+    create(model, dom) {
+        this.$search = $(this.search);
+        this.$searchContainer = $(this.searchContainer);
+        this.bind(this.parent.core);
+    }
 
-    this.$searchContainer = $search;
-    this.$search = $search.find('input');
+    bind(core) {
+        var self = this;
 
-    $rendered.prepend($search);
+        var resultsId = core.id + '-results';
 
-    return $rendered;
-};
+        this.$search.on('keydown', function(evt) {
+            self.emit('keypress', evt);
 
-Search.prototype.bind = function(decorated, container) {
-    var self = this;
+            self._keyUpPrevented = evt.isDefaultPrevented();
+        });
 
-    var resultsId = container.id + '-results';
+        // Workaround for browsers which do not support the `input` event
+        // This will prevent double-triggering of events for browsers which support
+        // both the `keyup` and `input` events.
+        this.$search.on('input', function(evt) {
+            // Unbind the duplicated `keyup` event
+            $(this).off('keyup');
+        });
 
-    decorated.call(this, container);
+        this.$search.on('keyup input', function(evt) {
+            self.handleSearch(evt);
+        });
 
-    this.$search.on('keydown', function(evt) {
-        self.emit('keypress', evt);
+        core.on('open', function() {
+            self.$search.attr('tabindex', 0);
+            self.$search.attr('aria-controls', resultsId);
 
-        self._keyUpPrevented = evt.isDefaultPrevented();
-    });
-
-    // Workaround for browsers which do not support the `input` event
-    // This will prevent double-triggering of events for browsers which support
-    // both the `keyup` and `input` events.
-    this.$search.on('input', function(evt) {
-        // Unbind the duplicated `keyup` event
-        $(this).off('keyup');
-    });
-
-    this.$search.on('keyup input', function(evt) {
-        self.handleSearch(evt);
-    });
-
-    container.on('open', function() {
-        self.$search.attr('tabindex', 0);
-        self.$search.attr('aria-controls', resultsId);
-
-        self.$search.trigger('focus');
-
-        window.setTimeout(function() {
             self.$search.trigger('focus');
-        }, 0);
-    });
 
-    container.on('close', function() {
-        self.$search.attr('tabindex', -1);
-        self.$search.removeAttr('aria-controls');
-        self.$search.removeAttr('aria-activedescendant');
+            window.setTimeout(function() {
+                self.$search.trigger('focus');
+            }, 0);
+        });
 
-        self.$search.val('');
-        self.$search.trigger('blur');
-    });
-
-    container.on('focus', function() {
-        if (!container.isOpen()) {
-            self.$search.trigger('focus');
-        }
-    });
-
-    container.on('results:all', function(params) {
-        if (params.query.term == null || params.query.term === '') {
-            var showSearch = self.showSearch(params);
-
-            if (showSearch) {
-                self.$searchContainer.removeClass('select2-search--hide');
-            } else {
-                self.$searchContainer.addClass('select2-search--hide');
-            }
-        }
-    });
-
-    container.on('results:focus', function(params) {
-        if (params.data._resultId) {
-            self.$search.attr('aria-activedescendant', params.data._resultId);
-        } else {
+        core.on('close', function() {
+            self.$search.attr('tabindex', -1);
+            self.$search.removeAttr('aria-controls');
             self.$search.removeAttr('aria-activedescendant');
-        }
-    });
-};
 
-Search.prototype.handleSearch = function(evt) {
-    if (!this._keyUpPrevented) {
-        var input = this.$search.val();
+            self.$search.val('');
+            self.$search.trigger('blur');
+        });
 
-        this.emit('query', {
-            term: input
+        core.on('focus', function() {
+            if (!core.isOpen()) {
+                self.$search.trigger('focus');
+            }
+        });
+
+        core.on('results:all', function(params) {
+            if (params.query.term == null || params.query.term === '') {
+                var showSearch = self.showSearch(params);
+
+                if (showSearch) {
+                    self.$searchContainer.removeClass('select2-search--hide');
+                } else {
+                    self.$searchContainer.addClass('select2-search--hide');
+                }
+            }
+        });
+
+        core.on('results:focus', function(params) {
+            if (params.data._resultId) {
+                self.$search.attr('aria-activedescendant', params.data._resultId);
+            } else {
+                self.$search.removeAttr('aria-activedescendant');
+            }
         });
     }
 
-    this._keyUpPrevented = false;
-};
+    handleSearch(evt) {
+        if (!this._keyUpPrevented) {
+            var input = this.$search.val();
 
-Search.prototype.showSearch = function(_, params) {
-    return true;
-};
+            this.emit('query', {
+                term: input
+            });
+        }
 
-module.exports = Search;
+        this._keyUpPrevented = false;
+    }
+
+    showSearch(_, params) {
+        return true;
+    }
+}
