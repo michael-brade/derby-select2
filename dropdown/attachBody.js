b98ac1737e3093b5945ee307d7d9c567c3022d11
Michael Brade <brade@kde.org>
2019-12-04 06:04:24 +0100

    convert attachBody to ES6

--- dropdown/attachBody.js
+++ dropdown/attachBody.js
@@ -1,219 +1,241 @@
-var $ = require('jquery');
-var Utils = require('../utils');
+import $ from 'jquery';
 
-function AttachBody(decorated, options) {
-    this.$dropdownParent = options.get('dropdownParent') || $(document.body);
 
-    decorated.call(this, options);
+function hasScroll(index, el) {
+    // Adapted from the function created by @ShadowScripter
+    // and adapted by @BillBarry on the Stack Exchange Code Review website.
+    // The original code can be found at
+    // http://codereview.stackexchange.com/q/13338
+    // and was designed to be used with the Sizzle selector engine.
+
+    const $el = $(el);
+    const overflowX = el.style.overflowX;
+    const overflowY = el.style.overflowY;
+
+    //Check both x and y declarations
+    if (overflowX === overflowY &&
+        (overflowY === 'hidden' || overflowY === 'visible')) {
+        return false;
+    }
+
+    if (overflowX === 'scroll' || overflowY === 'scroll') {
+        return true;
+    }
+
+    return ($el.innerHeight() < el.scrollHeight ||
+        $el.innerWidth() < el.scrollWidth);
 }
 
-AttachBody.prototype.bind = function(decorated, container) {
-    var self = this;
 
-    var setupResultsEvents = false;
+export default class AttachBody {
+    constructor(decorated, options) {
+        this.$dropdownParent = options.get('dropdownParent') || $(document.body);
 
-    decorated.call(this, container);
+        decorated.call(this, options);
+    }
 
-    container.on('open', function() {
-        self._showDropdown();
-        self._attachPositioningHandler(container);
+    bind(decorated, container) {
+        let setupResultsEvents = false;
 
-        if (!setupResultsEvents) {
-            setupResultsEvents = true;
+        decorated.call(this, container);
 
-            container.on('results:all', function() {
-                self._positionDropdown();
-                self._resizeDropdown();
+        container.on('open', () => {
+            this._showDropdown();
+            this._attachPositioningHandler(container);
+
+            if (!setupResultsEvents) {
+                setupResultsEvents = true;
+
+                container.on('results:all', () => {
+                    this._positionDropdown();
+                    this._resizeDropdown();
+                });
+
+                container.on('results:append', () => {
+                    this._positionDropdown();
+                    this._resizeDropdown();
+                });
+            }
+        });
+
+        container.on('close', () => {
+            this._hideDropdown();
+            this._detachPositioningHandler(container);
+        });
+
+        this.$dropdownContainer.on('mousedown', evt => {
+            evt.stopPropagation();
+        });
+    }
+
+    // TODO: this would actually have to be done in on('destroy')
+    destroy(decorated) {
+        decorated.call(this);
+
+        this.$dropdownContainer.remove();
+    }
+
+    position(decorated, $dropdown, $container) {
+        // Clone all of the container classes
+        $dropdown.attr('class', $container.attr('class'));
+
+        $dropdown.removeClass('select2');
+        $dropdown.addClass('select2-container--open');
+
+        $dropdown.css({
+            position: 'absolute',
+            top: -999999
+        });
+
+        this.$container = $container;
+    }
+
+    render(decorated) {
+        const $container = $('<span></span>');
+
+        const $dropdown = decorated.call(this);
+        $container.append($dropdown);
+
+        this.$dropdownContainer = $container;
+
+        return $container;
+    }
+
+    _hideDropdown(decorated) {
+        this.$dropdownContainer.detach();
+    }
+
+    _attachPositioningHandler(decorated, container) {
+        const scrollEvent = `scroll.select2.${container.id}`;
+        const resizeEvent = `resize.select2.${container.id}`;
+        const orientationEvent = `orientationchange.select2.${container.id}`;
+
+        const $watchers = this.$container.parents().filter(hasScroll);
+        $watchers.each(function() {
+            $(this).data('select2-scroll-position', {
+                x: $(this).scrollLeft(),
+                y: $(this).scrollTop()
             });
+        });
 
-            container.on('results:append', function() {
-                self._positionDropdown();
-                self._resizeDropdown();
+        $watchers.on(scrollEvent, function(ev) {
+            const position = $(this).data('select2-scroll-position');
+            $(this).scrollTop(position.y);
+        });
+
+        $(window).on(`${scrollEvent} ${resizeEvent} ${orientationEvent}`,
+            e => {
+                this._positionDropdown();
+                this._resizeDropdown();
             });
+    }
+
+    _detachPositioningHandler(decorated, container) {
+        const scrollEvent = `scroll.select2.${container.id}`;
+        const resizeEvent = `resize.select2.${container.id}`;
+        const orientationEvent = `orientationchange.select2.${container.id}`;
+
+        const $watchers = this.$container.parents().filter(hasScroll);
+        $watchers.off(scrollEvent);
+
+        $(window).off(`${scrollEvent} ${resizeEvent} ${orientationEvent}`);
+    }
+
+    _positionDropdown() {
+        const $window = $(window);
+
+        const isCurrentlyAbove = this.$dropdown.hasClass('select2-dropdown--above');
+        const isCurrentlyBelow = this.$dropdown.hasClass('select2-dropdown--below');
+
+        let newDirection = null;
+
+        const offset = this.$container.offset();
+
+        offset.bottom = offset.top + this.$container.outerHeight(false);
+
+        const container = {
+            height: this.$container.outerHeight(false)
+        };
+
+        container.top = offset.top;
+        container.bottom = offset.top + container.height;
+
+        const dropdown = {
+            height: this.$dropdown.outerHeight(false)
+        };
+
+        const viewport = {
+            top: $window.scrollTop(),
+            bottom: $window.scrollTop() + $window.height()
+        };
+
+        const enoughRoomAbove = viewport.top < (offset.top - dropdown.height);
+        const enoughRoomBelow = viewport.bottom > (offset.bottom + dropdown.height);
+
+        const css = {
+            left: offset.left,
+            top: container.bottom
+        };
+
+        // Determine what the parent element is to use for calciulating the offset
+        let $offsetParent = this.$dropdownParent;
+
+        // For statically positoned elements, we need to get the element
+        // that is determining the offset
+        if ($offsetParent.css('position') === 'static') {
+            $offsetParent = $offsetParent.offsetParent();
         }
-    });
 
-    container.on('close', function() {
-        self._hideDropdown();
-        self._detachPositioningHandler(container);
-    });
+        const parentOffset = $offsetParent.offset();
 
-    this.$dropdownContainer.on('mousedown', function(evt) {
-        evt.stopPropagation();
-    });
-};
+        css.top -= parentOffset.top;
+        css.left -= parentOffset.left;
 
-// TODO: this would actually have to be done in on('destroy')
-AttachBody.prototype.destroy = function(decorated) {
-    decorated.call(this);
+        if (!isCurrentlyAbove && !isCurrentlyBelow) {
+            newDirection = 'below';
+        }
 
-    this.$dropdownContainer.remove();
-};
+        if (!enoughRoomBelow && enoughRoomAbove && !isCurrentlyAbove) {
+            newDirection = 'above';
+        } else if (!enoughRoomAbove && enoughRoomBelow && isCurrentlyAbove) {
+            newDirection = 'below';
+        }
 
-AttachBody.prototype.position = function(decorated, $dropdown, $container) {
-    // Clone all of the container classes
-    $dropdown.attr('class', $container.attr('class'));
+        if (newDirection == 'above' ||
+            (isCurrentlyAbove && newDirection !== 'below')) {
+            css.top = container.top - parentOffset.top - dropdown.height;
+        }
 
-    $dropdown.removeClass('select2');
-    $dropdown.addClass('select2-container--open');
+        if (newDirection != null) {
+            this.$dropdown
+                .removeClass('select2-dropdown--below select2-dropdown--above')
+                .addClass(`select2-dropdown--${newDirection}`);
+            this.$container
+                .removeClass('select2-container--below select2-container--above')
+                .addClass(`select2-container--${newDirection}`);
+        }
 
-    $dropdown.css({
-        position: 'absolute',
-        top: -999999
-    });
-
-    this.$container = $container;
-};
-
-AttachBody.prototype.render = function(decorated) {
-    var $container = $('<span></span>');
-
-    var $dropdown = decorated.call(this);
-    $container.append($dropdown);
-
-    this.$dropdownContainer = $container;
-
-    return $container;
-};
-
-AttachBody.prototype._hideDropdown = function(decorated) {
-    this.$dropdownContainer.detach();
-};
-
-AttachBody.prototype._attachPositioningHandler = function(decorated, container) {
-    var self = this;
-
-    var scrollEvent = 'scroll.select2.' + container.id;
-    var resizeEvent = 'resize.select2.' + container.id;
-    var orientationEvent = 'orientationchange.select2.' + container.id;
-
-    var $watchers = this.$container.parents().filter(Utils.hasScroll);
-    $watchers.each(function() {
-        $(this).data('select2-scroll-position', {
-            x: $(this).scrollLeft(),
-            y: $(this).scrollTop()
-        });
-    });
-
-    $watchers.on(scrollEvent, function(ev) {
-        var position = $(this).data('select2-scroll-position');
-        $(this).scrollTop(position.y);
-    });
-
-    $(window).on(scrollEvent + ' ' + resizeEvent + ' ' + orientationEvent,
-        function(e) {
-            self._positionDropdown();
-            self._resizeDropdown();
-        });
-};
-
-AttachBody.prototype._detachPositioningHandler = function(decorated, container) {
-    var scrollEvent = 'scroll.select2.' + container.id;
-    var resizeEvent = 'resize.select2.' + container.id;
-    var orientationEvent = 'orientationchange.select2.' + container.id;
-
-    var $watchers = this.$container.parents().filter(Utils.hasScroll);
-    $watchers.off(scrollEvent);
-
-    $(window).off(scrollEvent + ' ' + resizeEvent + ' ' + orientationEvent);
-};
-
-AttachBody.prototype._positionDropdown = function() {
-    var $window = $(window);
-
-    var isCurrentlyAbove = this.$dropdown.hasClass('select2-dropdown--above');
-    var isCurrentlyBelow = this.$dropdown.hasClass('select2-dropdown--below');
-
-    var newDirection = null;
-
-    var offset = this.$container.offset();
-
-    offset.bottom = offset.top + this.$container.outerHeight(false);
-
-    var container = {
-        height: this.$container.outerHeight(false)
-    };
-
-    container.top = offset.top;
-    container.bottom = offset.top + container.height;
-
-    var dropdown = {
-        height: this.$dropdown.outerHeight(false)
-    };
-
-    var viewport = {
-        top: $window.scrollTop(),
-        bottom: $window.scrollTop() + $window.height()
-    };
-
-    var enoughRoomAbove = viewport.top < (offset.top - dropdown.height);
-    var enoughRoomBelow = viewport.bottom > (offset.bottom + dropdown.height);
-
-    var css = {
-        left: offset.left,
-        top: container.bottom
-    };
-
-    // Determine what the parent element is to use for calciulating the offset
-    var $offsetParent = this.$dropdownParent;
-
-    // For statically positoned elements, we need to get the element
-    // that is determining the offset
-    if ($offsetParent.css('position') === 'static') {
-        $offsetParent = $offsetParent.offsetParent();
+        this.$dropdownContainer.css(css);
     }
 
-    var parentOffset = $offsetParent.offset();
+    _resizeDropdown() {
+        const css = {
+            width: `${this.$container.outerWidth(false)}px`
+        };
 
-    css.top -= parentOffset.top;
-    css.left -= parentOffset.left;
+        if (this.options.get('dropdownAutoWidth')) {
+            css.minWidth = css.width;
+            css.position = 'relative';
+            css.width = 'auto';
+        }
 
-    if (!isCurrentlyAbove && !isCurrentlyBelow) {
-        newDirection = 'below';
+        this.$dropdown.css(css);
     }
 
-    if (!enoughRoomBelow && enoughRoomAbove && !isCurrentlyAbove) {
-        newDirection = 'above';
-    } else if (!enoughRoomAbove && enoughRoomBelow && isCurrentlyAbove) {
-        newDirection = 'below';
+    _showDropdown(decorated) {
+        this.$dropdownContainer.appendTo(this.$dropdownParent);
+
+        this._positionDropdown();
+        this._resizeDropdown();
     }
-
-    if (newDirection == 'above' ||
-        (isCurrentlyAbove && newDirection !== 'below')) {
-        css.top = container.top - parentOffset.top - dropdown.height;
-    }
-
-    if (newDirection != null) {
-        this.$dropdown
-            .removeClass('select2-dropdown--below select2-dropdown--above')
-            .addClass('select2-dropdown--' + newDirection);
-        this.$container
-            .removeClass('select2-container--below select2-container--above')
-            .addClass('select2-container--' + newDirection);
-    }
-
-    this.$dropdownContainer.css(css);
-};
-
-AttachBody.prototype._resizeDropdown = function() {
-    var css = {
-        width: this.$container.outerWidth(false) + 'px'
-    };
-
-    if (this.options.get('dropdownAutoWidth')) {
-        css.minWidth = css.width;
-        css.position = 'relative';
-        css.width = 'auto';
-    }
-
-    this.$dropdown.css(css);
-};
-
-AttachBody.prototype._showDropdown = function(decorated) {
-    this.$dropdownContainer.appendTo(this.$dropdownParent);
-
-    this._positionDropdown();
-    this._resizeDropdown();
-};
-
-module.exports = AttachBody;
+}
